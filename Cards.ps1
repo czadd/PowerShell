8e989b342aba77c6381fa57256ee5dfa8987045c
Chad R. Smith <chad.smith@ncm.com>
2016-02-02 12:19:22 -0700

    First pass at the card shuffler

---
+++ Cards.ps1
@@ -0,0 +1,56 @@
+﻿Function New-CardSuit{
+    $CardsPerSuit = @()
+    $CardsPerSuit += [PsCustomObject]@{Name='A';Value=1;AltValue=11}
+    $CardsPerSuit += [PsCustomObject]@{Name='K';Value=10;AltValue=10}
+    $CardsPerSuit += [PsCustomObject]@{Name='Q';Value=10;AltValue=10}
+    $CardsPerSuit += [PsCustomObject]@{Name='J';Value=10;AltValue=10}
+    2..10 | Foreach{ $CardsPerSuit += [PsCustomObject]@{Name=$_.tostring();Value=$_;AltValue=$_} }
+    $CardsPerSuit | sort value, name
+}
+
+Function Remove-card { 
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)][Object]$CardSet,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)][String]$CurrentCard
+    )
+    $CardSet = $CardSet | Where { $_ -notmatch $CurrentCard }
+    $CardSet
+}
+
+Function Get-TopCard( $CardSet ){
+    $CardSet | select -First 1
+}
+
+Function Start-Shuffle ($CardSet) {
+    $TotalCardCount = $Cardset.count
+    $NewCardSet = @()
+    $i=0
+    Do{
+        $i++
+        $CurrentCard = $CardSet | Get-Random
+        $NewCardSet += $CurrentCard
+        $CardSet = $CardSet | Where { $_ -notmatch $CurrentCard }
+    }
+    Until( $i -ge $TotalCardCount)
+    $NewCardSet
+}
+
+$Suit = 'Spades','Hearts','Clubs','Diamonds'
+
+$Deck = Foreach( $S in $Suit ){
+    Foreach( $Card in New-CardSuit ){
+        $Card | Select-Object -Property *,@{label='Suit';Expression={$S}}
+    }
+}
+
+$Deck = Start-Shuffle $Deck
+
+$TotalCardCount = $Deck.count
+
+$ShuffledDeck = For( $i = 0; $i -lt $TotalCardCount; $i++ ){
+    $TopCard = Get-TopCard $Deck
+    $Deck = Remove-card $Deck $Topcard
+    [PscustomObject] @{Name = $TopCard.Name;Suit=$TopCard.Suit}
+}
+$ShuffledDeck | ft -AutoSize
